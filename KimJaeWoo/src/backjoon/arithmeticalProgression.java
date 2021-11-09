54b6b2637cec135185af8e2641e8964bbeb78230
KimJaeWoo <68371941+rlawodnv2@users.noreply.github.com>
2021-11-09 13:33:09 +0900

    백준 한수
    1065번: 한수

---
+++ KimJaeWoo/src/backjoon/arithmeticalProgression.java
@@ -0,0 +1,37 @@
+import java.io.*;
+
+public class arithmeticalProgression {
+  
+  public static int hanNumber(int N) {
+    int result = 0;
+    
+    if(N<100){
+     return N;
+    } else {
+      return = 99;
+      
+      if(N == 100){
+        N = 999; 
+      }
+      
+      for(int i = 100; i <= N; i++){
+        int thirdNum = i/100;
+        int secondNum = (i/10)%10;
+        int firstNum = i%10;
+        
+        if((thirdNum - secondNum) == (secondNum - firstNum)) {
+          result++; 
+        }
+      }
+      return result;
+    }
+  }
+  
+  public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    int N = Integer.parseInt(br.readLine());
+    
+    System.out.println(hanNumber(N));
+  }
+}

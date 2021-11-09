9891a75b0337190305a68f579993fe5b405ab984
KimJaeWoo <68371941+rlawodnv2@users.noreply.github.com>
2021-11-09 14:54:14 +0900

    Update and rename phoneBook to PhoneBook.java

--- KimJaeWoo/src/backjoon/PhoneBook.java
+++ KimJaeWoo/src/backjoon/PhoneBook.java
@@ -1,7 +1,7 @@
 import java.util.HashMap;
 import java.util.Map;
 
-class phoneBook {
+class PhoneBook {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
         

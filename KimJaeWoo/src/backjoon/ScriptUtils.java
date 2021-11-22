dea1dc71008fcc52f3a262023b9dcc5e7ea2c066
KimJaeWoo <68371941+rlawodnv2@users.noreply.github.com>
2021-11-22 10:09:32 +0900

    controller script 유틸
    2021.11.20

---
+++ KimJaeWoo/src/backjoon/ScriptUtils.java
@@ -0,0 +1,36 @@
+package kr.co.etbs.xanadumall.common.util;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+
+public class ScriptUtils {
+	public static void init(HttpServletResponse response) {
+		response.setContentType("text/html; charset=utf-8");
+		response.setCharacterEncoding("utf-8");
+	}
+	
+	public static void alert(HttpServletResponse response, String alertMsg) throws IOException {
+		init(response);
+		PrintWriter out = response.getWriter();
+		out.println("<script>alert('"+alertMsg+"');</script>");
+		out.flush();
+	}
+	
+	//try에서 사용
+	public static void alertAndMovePage(HttpServletResponse response, String alertMsg, String nextPage) throws IOException {
+		init(response);
+		PrintWriter out = response.getWriter();
+		out.println("<script>alert('"+alertMsg+"'); location.href='"+nextPage+"';</script>");
+		out.flush();
+	}
+	
+	//catch에서 사용
+	public static void alertAndBackPage(HttpServletResponse response, String alertMsg) throws IOException {
+		init(response);
+		PrintWriter out = response.getWriter();
+		out.println("<script>alert('"+alertMsg+"'); history.go(-1);</script>");
+		out.flush();
+	}
+}
